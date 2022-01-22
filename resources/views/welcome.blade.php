439d33a73b756eb8a3d424819282826bc64012ba
Joshua <xeon826@gmail.com>
2022-01-22 17:36:34 -0600

    Minor visual changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,8 +1,8 @@
 @extends('layout')
 @section('title', 'Welcome')
 @section('content')
-  <div id='welcome'>
-    <a href='/wp_posts'><h1>10 Most Recent Wordpress Posts</h1></a>
-    <a href='/places'><h1>Places Auto Complete With Google Maps</h1></a>
+  <div id='welcome' class='flex'>
+    <a href='/wp_posts' class='grow text-center text-sky-500 text-3xl hover:underline'><h1>10 Most Recent Wordpress Posts</h1></a>
+    <a href='/places' class='grow text-center text-sky-500 text-3xl hover:underline'><h1>Places Auto Complete With Google Maps</h1></a>
   </div>
 @endsection
